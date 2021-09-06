2bfe96c1d46e56ccb5d0edd27f4cf779244571d3
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-06 05:46:46 +0300

    Initial Commit

---
+++ settings app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  settings app
+//
+//  Created by Abdirizak Hassan on 9/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
